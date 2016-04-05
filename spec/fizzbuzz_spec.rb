aebace32795a787bbd0a9c23c88375231212987d
Jack Hardy <jackhardy1@gmail.com>
2016-04-05 12:57:57 +0100

    Contexts added

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,29 +3,35 @@ require "fizzbuzz"
 describe Fizzbuzz do
   let(:fizzbuzz) {described_class.new}
 
-describe "argument check" do
-  it {is_expected.to respond_to(:function).with(1).argument}
-end
+  describe "#function" do
+    context "Numbers divisible by 3" do
+    it "Will return 'fizz'" do
+      expect(fizzbuzz.function(3)).to eq "fizz"
+    end
 
-describe "#function" do
-  it "It will return 'fizz' when given a multiple of 3" do
-    expect(fizzbuzz.function(3)).to eq "fizz"
-end
+  it "Will not return 'fizz'" do
+    expect(fizzbuzz.function(7)).not_to eq "fizz"
+    end
+  end
 
-it "It wlll not return fizz if number is not divisible by 3" do
-  expect(fizzbuzz.function(7)).not_to eq "fizz"
-end
+  context "Numbers divisible by 5" do
+  it "Will return 'buzz'" do
+    expect(fizzbuzz.function(5)).to eq "buzz"
+  end
 
-it "It will return buzz if number is divisible by 5" do
-  expect(fizzbuzz.function(5)).to eq "buzz"
-end
+  it "Will not return 'buzz'" do
+    expect(fizzbuzz.function(4)).not_to eq "buzz"
+    end
+  end
 
-it "It will not return buzz if number is not divisible by 5" do
-  expect(fizzbuzz.function(4)).not_to eq "buzz"
-end
+  context "Numbers divisible by both 3 and 5" do
+  it "Will return 'FizzBuzz'" do
+    expect(fizzbuzz.function(15)).to eq "FizzBuzz"
+  end
 
-it "It will return 'FizzBuzz' when number is divisible by both 3 and 5" do
-  expect(fizzbuzz.function(15)).to eq "FizzBuzz"
-end
+  it "Will not return 'Fizzbuzz'" do
+    expect(fizzbuzz.function(20)).not_to eq "Fizzbuzz"
+    end
+  end
 end
 end
